d8ff7cbdb2446bb0db2cdd0e1c377e36faa33306
Antonin Islam <antonin686.dev@gmail.com>
2025-02-25 08:18:54 +0600

    initial setup with astronvim

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "tsx",
+    },
+  },
+}
