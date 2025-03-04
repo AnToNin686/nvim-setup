a4557c1562cd57938d3a2be79966883b46b2cf9e
Antonin Islam <antonin686.dev@gmail.com>
2025-03-04 12:06:39 +0600

    initial setup

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "lua-language-server",
+        "eslint_d",
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+}
